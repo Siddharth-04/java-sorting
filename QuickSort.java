4b2b0f2a949e4df2152c7b33f36e71a3ec96e592
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-03-24 09:38:20 +0530

    [Siddharth] ADD: Quick Sort Java Program

---
+++ QuickSort.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= j && arr[i] <= pivot) i++;
+            while (i <= j && arr[j] > pivot) j--;
+            if (i < j) swap(arr, i, j);
+        }
+
+        swap(arr, low, j);
+        return j;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
